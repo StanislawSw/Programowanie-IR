7ff8b5554c0664ef4384bf69636e693e993979a2
StanislawSw <swierczewski.stanislaw@gmail.com>
2021-03-13 15:30:30 +0100

    proba

--- Funkcje.py
+++ Funkcje.py
@@ -1,13 +1,7 @@
 import math
-import cmath
-a = float(input("\ta = ".expandtabs(3)))
-b = float(input("\tb = ".expandtabs(3)))
-c = float(input("\tc = ".expandtabs(3)))
-print()
-delata = (b**2)-(4*a*c)
-if delta == 0:
-    x0 = -b / (2*a)
-    print("Rozwiązanie: \n\tx0 = {:.2f}".format(x0).expandtabs(3))
-    elif delta > 0:
-        x1 = (-b - math.sqrt(delta))/(2*a)
+a = float(input("podaj warotści parametru a"))
+b = float(input("podaj warotści parametru b"))
+x = -b/a
+print(x)
+
     
